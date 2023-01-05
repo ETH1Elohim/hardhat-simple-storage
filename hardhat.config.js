3f2158e7a644fa4beadc8876a5eb8fa7001f68b8
ETH1Elohim <ezmax.defi@gmail.com>
2023-01-05 12:45:20 -0700

    include gas report

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,6 +7,7 @@ require("hardhat-gas-reporter")
 const GOERLI_RPC_URL = process.env.GOERLI_RPC_URL
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
@@ -30,6 +31,10 @@ module.exports = {
     },
     gasReporter: {
         enabled: true,
+        outputFile: "gas-report.txt",
+        noColors: true,
+        currency: "USD",
+        coinmarketcap: COINMARKETCAP_API_KEY,
     },
 }
 
