68483cd35e5bd45c7ece6f23b4e9de6a96bbcd0b
ETH1Elohim <ezmax.defi@gmail.com>
2023-01-05 12:36:01 -0700

    testing

---
+++ test/test-deploy.js
@@ -0,0 +1,36 @@
+const { ethers } = require("hardhat")
+const { expect, assert } = require("chai")
+
+// hardhat works with mocha
+describe("SimpleStorage", function () {
+    let simpleStorageFactory, simpleStorage
+    beforeEach(async function () {
+        simpleStorageFactory = await ethers.getContractFactory("SimpleStorage")
+        simpleStorage = await simpleStorageFactory.deploy()
+    })
+
+    // it(what test should do, & code)
+    it("Should start with a favorite number of 0", async function () {
+        const currentValue = await simpleStorage.retrieve()
+        const expectedValue = "0"
+        assert.equal(currentValue.toString(), expectedValue)
+        // expect(currentValue.toString()).to.equal(expectedValue)
+    })
+
+    it("Should update when we call store", async function () {
+        const expectedValue = "17"
+        const transactionResponse = await simpleStorage.store(expectedValue)
+        await transactionResponse.wait(1)
+
+        const currentValue = await simpleStorage.retrieve()
+        assert.equal(currentValue.toString(), expectedValue)
+    })
+
+    // describe("something", () => {
+    //   beforeEach()
+
+    //   it()
+    //   it()
+    //   it()
+    // })
+})
